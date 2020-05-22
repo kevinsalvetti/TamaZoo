e6c0e960e269e34192d31c11ce2a2750d112f7da
kevin salvetti <k.salvetti@studenti.unibs.itgit config --global user.name kevinsalvetti>
2020-05-22 14:39:40 +0200

    Creazione classe TamaZoo

--- src/tamaZoo/TamaMain.java
+++ src/tamaZoo/TamaMain.java
@@ -19,6 +19,13 @@ public class TamaMain {
 	private final static int TAMA_GORDO = 3;
 	
 	
+	private final static int MIN_BISCOTTI = 1;
+	private final static int MAX_BISCOTTI = 8;
+
+	private final static int MIN_CAREZZE = 1;
+	private final static int MAX_CAREZZE = 10;
+	
+	
 	private static TamaZoo Zoo = new TamaZoo();
 	
 	public static void main(String[] args) {
@@ -29,18 +36,54 @@ public class TamaMain {
 	int numeroTamagotchi = InputDati.leggiInteroPositivo(INSERT_TAMAGOTCHI);
 	
 	for(int i = 1 ; i <= numeroTamagotchi; i++) {
+		
 		Tamagotchi nuovoTama = creaTamagotchi();
+		
 		Zoo.inserisci(nuovoTama);
 	}
 		
+	MyMenu principale = new MyMenu(MAIN_QUESTION, MAIN_MENU_ITEMS);
+
+	boolean fine = false;
+
+	do {
+		int voceSelezionata = principale.scegli();
+
+		switch (voceSelezionata) {
+		case 1:
+			int numBiscotti = NumeriCasuali.estraiIntero(MIN_BISCOTTI, MAX_BISCOTTI);
+			System.out.printf(MSG_BISCOTTI, numBiscotti);
+			Zoo.daiBiscotti(numBiscotti);
+			break;
+		case 2:
+			int numCarezze = NumeriCasuali.estraiIntero(MIN_CAREZZE, MAX_CAREZZE);
+			System.out.printf(MSG_CAREZZE, numCarezze);
+			Zoo.daiCarezze(numCarezze);
+			break;
+		case 0:
+			fine = true;
+			System.out.println(MSG_SALUTI);
+			break;
+		default:
+			System.out.println(MSG_NO_OP);
+			break;
+		}// switch
+
+		if (!mioZoo.ciSonoVivi()) {
+			fine = true;
+			System.out.println(MSG_ESTINTI);
+		}
+
+	} while (!fine);
 	
-	
-	public Tamagotchi creaTamagotchi() {
+	}
+
+	private static Tamagotchi creaTamagotchi() {
 		
 		String nome = InputDati.leggiStringaNonVuota(INSER_NOME_TAMA);
 		int specie = NumeriCasuali.estraiIntero(1, NUMERO_SPECIE);
 		
-		Tamagotchi nuovo = null;
+		Tamagotchi nuovoTama = null;
 		
 		int affetto, sazieta;
 
@@ -48,21 +91,19 @@ public class TamaMain {
 		case TAMA_BASE:
 			affetto = NumeriCasuali.estraiIntero(0, Tamagotchi.MAX_SODDISFAZIONE);
 			sazieta = NumeriCasuali.estraiIntero(0, Tamagotchi.MAX_SAZIETA);
-			nuovo = new Tamagotchi(nome, affetto, sazieta);
+			nuovoTama = new Tamagotchi(nome, affetto, sazieta);
 			break;
 		case TAMA_TRISTE:
 			sazieta = NumeriCasuali.estraiIntero(0, Tamagotchi.MAX_SAZIETA);
-			nuovo = new TamaTriste(nome, sazieta);
+			nuovoTama = new TamaTriste(nome, sazieta);
 			break;
 		case TAMA_GORDO:
 			sazieta = NumeriCasuali.estraiIntero(0, Tamagotchi.MAX_SAZIETA);
-			nuovo = new TamaGordo(nome, sazieta);
+			nuovoTama = new TamaGordo(nome, sazieta);
 			break;
 		}
 		return nuovoTama;
-	}
-	
-	
+		
 	}
 
 }
--- src/tamaZoo/TamaZoo.java
+++ src/tamaZoo/TamaZoo.java
@@ -1,5 +1,60 @@
 package tamaZoo;
 
+import java.util.*;
+
+
+
 public class TamaZoo {
 
+	private ArrayList <Tamagotchi> specie;
+	
+	
+	public TamaZoo() {
+		
+		specie = new ArrayList <Tamagotchi> ();
+	}
+	
+	public void inserisci (Tamagotchi Inserire)
+	{
+		specie.add(Inserire);
+	}
+	
+	public boolean controlloVivi() {
+		
+		for(Tamagotchi elemento : specie)
+		{
+			if (! elemento.isDie())
+				return true;
+		}
+		return false;
+	}
+	
+	public void daiBiscotti (int quantiBiscotti)
+	  {
+		  for (Tamagotchi elemento : specie)
+		  {
+			  if (! elemento.isDie())
+			  {
+			   elemento.daiBiscotti(quantiBiscotti);
+			   System.out.println(elemento.toString());
+			   System.out.println();
+			  }
+			  
+		  }
+	  }
+	  
+	  public void daiCarezze (int quanteCarezze)
+	  {
+		  for (Tamagotchi elemento : specie)
+		  {
+			  if (! elemento.isDie())
+		  	  {
+			   elemento.aggiungiCarezze(quanteCarezze);
+			   System.out.println(elemento.toString());
+			   System.out.println();
+		  	  }
+			  
+		  }
+	  }
+	
 }
