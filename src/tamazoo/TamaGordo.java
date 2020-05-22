630dd958650e9dff4af2b0a890a6e8f806d20b8d
MicheleMozzanica <mozzo2000@gmail.com>
2020-05-22 11:05:56 +0200

    creazione tamagordo e tamatriste

--- src/tamazoo/TamaGordo.java
+++ src/tamazoo/TamaGordo.java
@@ -1,5 +1,29 @@
 package tamazoo;
 
-public class TamaGordo {
+public class TamaGordo extends Tamagotchi {
 
+	private final static int MOLTIPLICATORE_CAREZZE = 2;
+
+	public TamaGordo(String nome, int sazieta) {
+		super(nome, MAX_AFFETTO, sazieta);
+		super.nomeTipo = "gordo";
+}
+
+	public void riceviCarezze(int, numCarezze) {
+		sazieta (Math.max(0, getSazieta() - ((numCarezze * MOLTIPLICATORE_CAREZZE) / FATTORE_CAREZZE)));
+	}
+
+	public void riceviBiscotti(int, numBiscotti) {
+		for (int i = 1; i <= numBiscotti; i++) {
+		sazieta ( Math.min(szieta() * INCREMENTA_BISCOTTI , MAX_SAZIETA));
+		}
+	}
+	
+	public boolean isDie() {
+		return sazieta() == 0;
+	}
+
+	public boolean isInfelice() {
+		return sazieta() < SOGLIA_MIN_SAZIETA;
+	}
 }
